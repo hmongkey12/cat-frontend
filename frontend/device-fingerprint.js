fef5d5c558179b052e948ca57f31db1d8943ed8d
Phil Schmeiser <pschmeis@amazon.com>
2022-05-03 17:20:07 -0500

    initial device fingerprinting logic

---
+++ frontend/device-fingerprint.js
@@ -0,0 +1,35 @@
+async function deviceHash() {
+  let nav = window.navigator;
+  let vars = [
+    // Consistent across Chrome/Mozilla
+    nav.platform,
+    nav.hardwareConcurrency,
+    nav.language,
+    nav.maxTouchPoints,
+  ];
+
+  // Unique to Chromium/Mozilla
+  let devicesObject = [];
+  (async () => {
+    let deviceResult = await nav.mediaDevices.enumerateDevices();
+    devicesObject.push(deviceResult);
+  })();
+  let deviceIDs = devicesObject[0].map((a) => a.groupID);
+
+  // Create finished array for hashing
+  let finalVars = vars.concat(deviceIDs);
+
+  // Do the hash
+  let varsEncode = new TextEncoder().encode(finalVars);
+  let varsHashBuffer = await crypto.subtle.digest("SHA-256", varsEncode);
+  let varHashArray = Array.from(new Uint8Array(varsHashBuffer));
+  let varHex = varHashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+
+  return varHex;
+}
+
+module.exports = {
+  deviceHash,
+};
