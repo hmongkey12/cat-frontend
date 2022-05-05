020679d7360d48a4285fa615e48841475d247b89
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-05-05 12:35:35 -0700

    created the smash to mash all of our hash together

---
+++ helpers/canvas.js
@@ -0,0 +1,46 @@
+// hashing md5 function
+function getCanvasHash() {
+  let src = "";
+  let hash = "";
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "rgb(255,0,255)";
+  ctx.beginPath();
+  ctx.rect(20, 20, 150, 100);
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
+  ctx.beginPath();
+  ctx.fillStyle = "rgb(0,255,255)";
+  ctx.arc(50, 50, 50, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.stroke();
+  ctx.closePath();
+
+  let txt = "abz190#$%^@£éú";
+  ctx.textBaseline = "top";
+  ctx.font = '17px "Arial 17"';
+  ctx.textBaseline = "alphabetic";
+  ctx.fillStyle = "rgb(255,5,5)";
+  // ctx.rotate(0.03);
+  ctx.fillText(txt, 4, 17);
+  ctx.fillStyle = "rgb(155,255,5)";
+  ctx.shadowBlur = 8;
+  ctx.shadowColor = "red";
+  ctx.fillRect(20, 12, 100, 5);
+  src = canvas.toDataURL();
+  // hash = sha256(src);
+  canvas.remove();
+
+  let char = "";
+  for (let i = 0; i < src.length; i++) {
+    char = src.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash;
+  }
+
+  return hash;
+}
+
+export { getCanvasHash };
--- helpers/handleVote.js
+++ helpers/handleVote.js
@@ -1,18 +1,18 @@
-import { webglHash } from "./webglHashing.js";
+import { smashIt } from "./theSmash.js";
 
 document.getElementById("vote1").addEventListener("click", async () => {
-  const hash = webglHash();
+  const hash = smashIt();
   const backendURL = `https://epic-cat-battles-of-history.herokuapp.com/users?hash=${hash}&cat=CatInTheHat`;
   const postRes = await fetch(backendURL, {
     method: "POST",
   });
   const getRes = await fetch(backendURL, { method: "GET" });
   const getData = await getRes.json();
-  document.getElementById(vote1display).textContent = getData[0].vote;
+  document.getElementById("vote1display").textContent = getData[0].vote;
 });
 
 document.getElementById("vote2").addEventListener("click", async () => {
-  const hash = webglHash();
+  const hash = smashIt();
   const backendURL = `https://epic-cat-battles-of-history.herokuapp.com/users?hash=${hash}&cat=Sylvester`;
   const postRes = await fetch(backendURL, {
     method: "POST",
