ebbee8b0f9d85129393db306c09eeb4764fd5dd2
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-05-05 00:39:10 -0700

    updated request methods

--- helpers/handleVote.js
+++ helpers/handleVote.js
@@ -16,10 +16,10 @@ document.getElementById("vote2").addEventListener("click", async () => {
   const hash = webglHash();
   const backendURL = `https://epic-cat-battles-of-history.herokuapp.com/users?hash=${hash}&cat=Sylvester`;
   const postRes = await fetch(backendURL, {
-    method: "GET",
+    method: "POST",
     mode: "no-cors",
   });
   const getRes = await fetch(backendURL, { method: "GET", mode: "no-cors" });
-  const getData = await getRes.json();
+  // const getData = await getRes.json();
   document.getElementById(vote2display).textContent = getData.vote;
 });
