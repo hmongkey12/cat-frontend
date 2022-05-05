eeb8ac1f57ceac32555ac4659bc5d123388d5b83
Khammadao Lee <khammadaolee@Khammadaos-MacBook-Pro.local>
2022-05-05 09:29:14 -0700

    fixed handleVote for both POST and GET requests

--- helpers/handleVote.js
+++ helpers/handleVote.js
@@ -5,11 +5,10 @@ document.getElementById("vote1").addEventListener("click", async () => {
   const backendURL = `https://epic-cat-battles-of-history.herokuapp.com/users?hash=${hash}&cat=CatInTheHat`;
   const postRes = await fetch(backendURL, {
     method: "POST",
-    mode: "no-cors",
   });
-  const getRes = await fetch(backendURL, { method: "GET", mode: "no-cors" });
+  const getRes = await fetch(backendURL, { method: "GET" });
   const getData = await getRes.json();
-  document.getElementById(vote1display).textContent = getData.vote;
+  document.getElementById(vote1display).textContent = getData[0].vote;
 });
 
 document.getElementById("vote2").addEventListener("click", async () => {
@@ -17,9 +16,8 @@ document.getElementById("vote2").addEventListener("click", async () => {
   const backendURL = `https://epic-cat-battles-of-history.herokuapp.com/users?hash=${hash}&cat=Sylvester`;
   const postRes = await fetch(backendURL, {
     method: "POST",
-    mode: "no-cors",
   });
-  const getRes = await fetch(backendURL, { method: "GET", mode: "no-cors" });
-  // const getData = await getRes.json();
-  document.getElementById(vote2display).textContent = getData.vote;
+  const getRes = await fetch(backendURL, { method: "GET" });
+  const getData = await getRes.json();
+  document.getElementById("vote2display").textContent = getData[0].vote;
 });
